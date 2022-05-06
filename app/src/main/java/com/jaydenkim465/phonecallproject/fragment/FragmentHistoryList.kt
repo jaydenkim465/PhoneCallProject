a48e136af15ab4b015308042d77396a24b1d26a6
Jayden_Kim <jaydenkim465@gmail.com>
2022-05-06 15:26:11 +0900

    통화목록 순차 조회 로직 추가

--- app/src/main/java/com/jaydenkim465/phonecallproject/fragment/FragmentHistoryList.kt
+++ app/src/main/java/com/jaydenkim465/phonecallproject/fragment/FragmentHistoryList.kt
@@ -24,19 +24,21 @@ import kotlin.math.*
  */
 class FragmentHistoryList() : Fragment() {
 	private var columnCount = 1
+	private var searchIndex = 0
+	private var loadingState = false
 	private var historyList = ArrayList<HistoryData>()
+	private lateinit var historyListAdapter:AdapterHistoryList
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		getHistoryList()
-
+		InitHistoryList()
 		val view = inflater.inflate(R.layout.fragment_history_list, container, false)
 
 		// RecyclerView Set the adapter
 		if (view is RecyclerView) {
-			val historyListAdapter = AdapterHistoryList(historyList)
+			historyListAdapter = AdapterHistoryList(historyList)
 
 			val swipeHelperCallBack = SwipeHelperCallBack().apply {
 				//setClamp(200f)
@@ -55,7 +57,6 @@ class FragmentHistoryList() : Fragment() {
 					false
 				}
 			}
-
 			with(view) {
 				layoutManager = when {
 					columnCount <= 1 -> LinearLayoutManager(context)
@@ -63,8 +64,29 @@ class FragmentHistoryList() : Fragment() {
 				}
 				adapter = historyListAdapter
 			}
+
+			view.setOnScrollChangeListener {v, scrollX, scrollY, oldScrollX, oldScrollY ->
+				var layoutManager:LinearLayoutManager = (view.layoutManager as LinearLayoutManager)
+				if(!loadingState) {
+					if(layoutManager != null && layoutManager.findLastCompletelyVisibleItemPosition() == historyList.size - 5) {
+						loadingState = true
+						loadHistory()
+					}
+					loadingState = false
+				}
+			}
 		}
 		return view
+	}
+
+	fun InitHistoryList() {
+		searchIndex = 0;
+		getHistoryList(searchIndex)
+	}
+
+	fun loadHistory() {
+		getHistoryList(searchIndex)
+		historyListAdapter.notifyDataSetChanged()
 	}
 
 	fun getHistoryList(index:Int) {
@@ -72,7 +94,17 @@ class FragmentHistoryList() : Fragment() {
 			return
 		}
 
-		val cursor = requireActivity().contentResolver.query(CallLog.Calls.CONTENT_URI, null, null, null, null)
+		// SQLite의 order by 이후에 ROWNUM 효과를 위한 구문
+		val afterOrderBy = "date DESC LIMIT ${index + 20} OFFSET $index"
+
+		/**
+		SELECT	date, transcription, photo_id, subscription_component_name, call_screening_app_name, type, geocoded_location, presentation, duration, subscription_id, is_read, number,
+		features, voicemail_uri, normalized_number, via_number, matched_number, last_modified, new, numberlabel, lookup_uri, data4, photo_uri, data3, data2, data1, data_usage,
+		phone_account_address, formatted_number, add_for_all_users, block_reason, numbertype, call_screening_component_name, countryiso, name, post_dial_digits, transcription_state, _id
+		FROM (SELECT * FROM calls WHERE logtype IN (100, 110, 150, 500, 800, 900, 950, 1000, 1050, 1100, 1150, 1350, 1400, 1450, 1500) ORDER BY date DESC ) calls
+		ORDER BY date DESC LIMIT 20 OFFSET 0
+		 */
+		val cursor = requireActivity().contentResolver.query(CallLog.Calls.CONTENT_URI, null, null, null, afterOrderBy)
 		val numberIndex = cursor?.getColumnIndex(CallLog.Calls.NUMBER)
 		val typeIndex = cursor?.getColumnIndex(CallLog.Calls.TYPE)
 		val dateIndex = cursor?.getColumnIndex(CallLog.Calls.DATE)
@@ -94,6 +126,7 @@ class FragmentHistoryList() : Fragment() {
 			)
 		}
 		cursor?.close()
+		searchIndex += 20
 	}
 
 	class SwipeHelperCallBack : ItemTouchHelper.Callback() {
